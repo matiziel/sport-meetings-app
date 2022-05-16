12102634e4c5cef5a47cba9b3422b33351c30485
matiziel <matiziel10@gmail.com>
2022-05-17 00:18:45 +0200

    Add User Model

--- SportMeetingsApi/Persistence/Database/DatabaseContext.cs
+++ SportMeetingsApi/Persistence/Database/DatabaseContext.cs
@@ -1,19 +1,19 @@
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-// namespace SportMeetingsApi.Persistence.Database {
-//     public class ApplicationDbContext : DbContext {
-//         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-//             : base(options) { }
+namespace SportMeetingsApi.Persistence.Database {
+    public class DatabaseContext : DbContext {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options) { }
 
-//         protected override void OnModelCreating(ModelBuilder modelBuilder) {
-//             base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
 
-//             modelBuilder.Entity<User>().HasKey(u => u.Id);
-//             modelBuilder.Entity<User>().Property(r => r.Email).IsRequired();
-//             modelBuilder.Entity<User>().HasIndex(r => r.Email).IsUnique();
-//             modelBuilder.Entity<User>().Property(r => r.PasswordHash).IsRequired();
-//         }
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(r => r.Email).IsRequired();
+            modelBuilder.Entity<User>().HasIndex(r => r.Email).IsUnique();
+            modelBuilder.Entity<User>().Property(r => r.PasswordHash).IsRequired();
+        }
 
-//         public DbSet<User> Users { get; set; }
-//     }
-// }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- SportMeetingsApi/Persistence/User.cs
+++ SportMeetingsApi/Persistence/User.cs
@@ -1,8 +1,10 @@
+#nullable enable
+
 namespace SportMeetingsApi.Persistence {
     public class User {
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Name { get; set; }
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string? Name { get; set; }
     }
 }
